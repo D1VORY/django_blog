7affc3587f9a69fbe2f5694372b0184ea15aa600
D1VORY <dubchakob@gmail.com>
2020-09-09 19:29:16 +0300

    created blog template

--- blog/views.py
+++ blog/views.py
@@ -7,5 +7,10 @@ class BlogListView(ListView):
     template_name = 'blog/blogs.html'
     model = Blog
 
+
+    def get_queryset(self):
+        blogs = Blog.objects.all().order_by('created')
+        return blogs
+
 def index(request):
     return render(request, 'blog/blogs.html')
--- django_blog/settings.py
+++ django_blog/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mainapp',
-    'blog'
+    'blog',
+    'columns'
 ]
 
 MIDDLEWARE = [
